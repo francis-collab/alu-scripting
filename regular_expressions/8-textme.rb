1ca268a89f71e331b2626a0ec8e380fe543ce8f5
francis-collab <francis-collab@UBIQUITOUS250>
2025-01-10 16:25:22 +0200

    Add script to extract sender, receiver, and flags from log entries

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,6 +1,2 @@
 #!/usr/bin/env ruby
-input = ARGV[0]
-sender = input.scan(/from:(\S+)/).flatten.first
-receiver = input.scan(/to:(\S+)/).flatten.first
-flags = input.scan(/flags:(\S+)/).flatten.first
-puts "#{sender},#{receiver},#{flags}"
+puts ARGV[0].scan(/(?<=from:|to:|flags:).+?(?=\])/).join(',')
